f53e7ff452f63d5a8306db10a4729b87e86075bb
David Bell <dtbell99@icloud.com>
2025-06-20 20:15:54 -0500

    card update

--- webapp/src/components/card.tsx
+++ webapp/src/components/card.tsx
@@ -16,7 +16,9 @@ const Card: React.FC<CardProps> = ({ title, imgHref, link, color }) => {
       <Link to={link} style={{ textDecoration: "none", color: "black" }}>
         <img src={imgHref} className="card-img-top" />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+          <h5 className="card-title" style={{ color: color }}>
+            {title}
+          </h5>
         </div>
       </Link>
     </div>
